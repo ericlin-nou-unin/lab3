f69a3328241feaed7f53b1d977043a5ee43e03e5
ericlin-nou-unin <zgwyjufa5081@outlook.com>
2025-10-20 12:42:50 +0800

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; // any version >=0.5.1 works
+
+contract HelloWorld {
+    function sayHello() public pure returns (string memory) {
+        return "Hello, world!";
+    }
+}
